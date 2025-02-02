e775b30c968fc9e35a76a445182c31f65a21599e
Uriel Ofir <urielofir@gmail.com>
2025-02-02 13:25:27 +0200

    add cors

--- app.js
+++ app.js
@@ -2,9 +2,17 @@
 
 import express from 'express';
 import {getGithubData} from './getGithubData.js';
+import cors from 'cors';
 
 // Create an Express application
 const app = express();
+
+
+const corsOptions = {
+  origin: 'http://localhost:4321/', // Allow only this origin
+  optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+};
+app.use(cors(corsOptions));
 
 // Define a port (use environment variable or default to 3000)
 const port = process.env.PORT || 3000;
@@ -12,6 +20,7 @@ const port = process.env.PORT || 3000;
 
 // Define a route for the root URL ("/")
 app.get('/', async (req, res) => {
+  const requestDate = new Date();
   res.send(await getGithubData());
 });
 

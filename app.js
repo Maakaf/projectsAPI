25c5320bbb9d7a8420c436640849ee8d3f77cd20
Uriel Ofir <urielofir@gmail.com>
2025-02-02 11:39:20 +0200

    first good run on my local host

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // Import the Express module
-const express = require('express');
+
+import express from 'express';
+import {getGithubData} from './getGithubData.js';
 
 // Create an Express application
 const app = express();
@@ -7,9 +9,10 @@ const app = express();
 // Define a port (use environment variable or default to 3000)
 const port = process.env.PORT || 3000;
 
+
 // Define a route for the root URL ("/")
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get('/', async (req, res) => {
+  res.send(await getGithubData());
 });
 
 // Start the server and listen on the specified port
---
+++ getGithubData.js
@@ -0,0 +1,78 @@
+import { Octokit } from "@octokit/rest";
+import projectData from "./projects.json" assert { type: "json" };
+import "dotenv/config";
+
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+
+// Fetch data from GitHub API
+export const getGithubData = async () => {
+  try {
+    const octokit = new Octokit({
+      auth: GITHUB_TOKEN,
+    });
+
+    async function getLastCommitDate(owner, repo, defaultBranch) {
+      try {
+        const commits = await octokit.rest.repos.listCommits({
+          owner,
+          repo,
+          sha: defaultBranch,
+          per_page: 1, // Only fetch the latest commit
+        });
+
+        if (commits.data.length > 0) {
+          const lastCommitDate = commits?.data[0]?.commit?.committer?.date;
+          return lastCommitDate;
+        } else {
+          throw new Error("No commits found in the default branch.");
+        }
+      } catch (error) {
+        console.error("Error fetching commit data:", error);
+        throw error;
+      }
+    }
+
+    const repositoriesData = await Promise.all(
+      projectData.map(async ({ githubLink, discordLink }) => {
+        try {
+          const [owner, repo] = githubLink.split("/").slice(-2);
+          const { data } = await octokit.repos.get({ owner, repo });
+
+          return {
+            name: data.name,
+            language: data.language || "N/A",
+            html_url: data.html_url,
+            description: data.description || "",
+            stargazers_count: data.stargazers_count || 0,
+            last_commit: await getLastCommitDate(
+              owner,
+              repo,
+              data.default_branch
+            ),
+            short_description: data.description
+              ? data.description.split("\n")[0]
+              : "",
+            discord_link: discordLink || "",
+          };
+        } catch (error) {
+          console.error("Error fetching repository data:", error);
+          return   {
+            name: 'N/A',
+            language: 'N/A',
+            html_url: 'N/A',
+            description: 'N/A',
+            stargazers_count: 0,
+            last_commit: 'N/A',
+            short_description: 'N/A',
+            discord_link: 'N/A'
+          };
+        }
+      })
+    );
+
+    console.log("Data fetched from GitHub:", repositoriesData);
+    return repositoriesData;
+  } catch (error) {
+    console.error("Error fetching data from GitHub:", error);
+  }
+};
